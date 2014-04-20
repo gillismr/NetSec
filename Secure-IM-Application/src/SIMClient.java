6ed7ab1d23b096ac27a54eccaa7d989108201638
Miles Gillis <gillis.miles@gmail.com>
2014-04-20 14:34:21 -0700

    I think I'm like... halfway done?
    This is intense. Been working since 9:15. 11 hours to go...

--- Secure-IM-Application/src/SIMClient.java
+++ Secure-IM-Application/src/SIMClient.java
@@ -3,20 +3,35 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.DatagramPacket;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.Scanner;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 /*
 import java.io.IOException;
@@ -34,30 +49,40 @@ import java.io.*;
 
 public class SIMClient {
 
-	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+	public static void main(String[] args) throws NoSuchAlgorithmException, IOException, InvalidKeySpecException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, NoSuchPaddingException {
     	
     	Socket server = null;
     	Socket recipient = null;
+    	ServerSocket listening = null;
+    	
     	BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+    	Scanner sc = null;
+    	
+    	byte[] serverKeyBytes = null;
     	byte[] sendData;
     	byte[] receiveData = new byte[1024];
-    	DataInputStream is = null;
-        DataOutputStream os = null;
+    	
+    	DataInputStream input = null;
+        DataOutputStream output = null;
         //DataInputStream inputLine = null;
-    	String message; 
+    	
+        String message; 
     	String name = "";
     	String password = "";
-    	Scanner sc = null;
     	//byte[] pwHash;
     	
     	try {
 			sc = new Scanner(new File("config.txt"));
-		} catch (FileNotFoundException e) {
-			System.out.println("Config file not found, please ensure a properly titled config.txt file is in the working directory");
+			serverKeyBytes = readByteFromFile(new File("pub1.class"));
+		} catch (Exception e) {
+			System.out.println("Config file and/or server public key not found, please ensure the properly titled config.txt and pub1.class files are in the working directory");
 			e.printStackTrace();
 		}
     	
-    	System.out.println("Configuring connection to server...");
+    	KeyFactory rsaKeyFactory = KeyFactory.getInstance("RSA");
+		X509EncodedKeySpec publicSpec = new X509EncodedKeySpec(serverKeyBytes);
+		PublicKey serverKey = rsaKeyFactory.generatePublic(publicSpec);
+    	System.out.println("Server info and public key found. Configuring connection to server...");
     	
     	try {
 			server = new Socket(sc.next(), Integer.parseInt(sc.next()));
@@ -68,7 +93,9 @@ public class SIMClient {
 			e.printStackTrace();
 		}
     	
-    	System.out.println("Connected to server. Login required.");
+    	output = new DataOutputStream(server.getOutputStream());
+    	input = new DataInputStream(server.getInputStream());
+    	System.out.println("Server connection and I/O streams established. Login required.");
     	
     	System.out.print("LOGIN NAME: ");
     	try {
@@ -91,13 +118,26 @@ public class SIMClient {
     	System.out.println("Nonce chosen.");
     	
     	MessageDigest md = MessageDigest.getInstance("SHA-512");
-    	byte [] pwHash = md.digest(password.getBytes());
+    	byte[] pwHash = md.digest(password.getBytes()); //64bytes = 512 bits
     	System.out.println("Password hashed.");
     	
     	sendData = makeLoginCreds(nonce1, pwHash, name);
-    	server.getOutputStream().write(sendData);
+    	System.out.println("Send data prepared.");
     	
-    	System.out.println("Credentials received, password hashed, nonce chosen. Initiating communication with server...");
+    	Cipher publicChiper = Cipher.getInstance("RSA");
+		publicChiper.init(Cipher.ENCRYPT_MODE, serverKey);
+		byte[] encryptedSendData = publicChiper.doFinal(sendData);
+		System.out.println("Send data encrypted.");
+		
+		output.write(encryptedSendData);
+    	System.out.println("Data sent. Dealing with cookies...");
+    	
+    	byte[] cookie = new byte[64]; // 64bytes = 512 bits
+    	input.readFully(cookie);
+    	output.write(cookie);
+    	
+    	//At this point, the server should start handling what we want to do exactly...
+    	
     	
     	
     	
@@ -112,6 +152,20 @@ public class SIMClient {
 		return outputStream.toByteArray();
 	}
 		
+	// read bytes from a file
+	public static byte[] readByteFromFile(File f) throws Exception {
+		if (f.length() > Integer.MAX_VALUE)
+			System.out.println("File is too large");
+		
+		byte[] buffer = new byte[(int) f.length()];
+		InputStream fis = new FileInputStream(f);;
+		DataInputStream dis = new DataInputStream(fis);
+		dis.readFully(buffer);
+		dis.close();
+		fis.close();
+		
+		return buffer;
+	}
 		
 		
 	
--- Secure-IM-Application/src/SIMServer.java
+++ Secure-IM-Application/src/SIMServer.java
@@ -1,9 +1,39 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketAddress;
+import java.security.GeneralSecurityException;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+
+import javax.crypto.Cipher;
+
 
 /*
 import java.io.IOException;
@@ -23,43 +53,168 @@ import java.io.*;
 
 public class SIMServer {
 	
-	//Initialize list of connected clients
-	List<SocketAddress> clients = new ArrayList<SocketAddress>();
-
-	//Initialize table of user names and PW hashes
-	//What data type? 
-	
-	//Initialize data buffers?
-	static byte[] sendData, receiveData;
-	
-	
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, GeneralSecurityException {
 		
-		//Initialize Socket and Packet?
 		@SuppressWarnings("resource")
-		DatagramSocket serverSocket = new DatagramSocket(Integer.parseInt(args[0]));
-		DatagramPacket packet = new DatagramPacket(receiveData, receiveData.length);
-		System.out.println("Server Initialized, listening on port " + serverSocket.getPort() + "...");
+		ServerSocket serverSocket = new ServerSocket(9090);
+		byte[] privateKeyBytes = readByteFromFile(new File("priv1.class"));
 		
+		KeyFactory rsaKeyFactory = KeyFactory.getInstance("RSA");
+		PKCS8EncodedKeySpec privateSpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+		PrivateKey privateKey = rsaKeyFactory.generatePrivate(privateSpec);
 		
+		Random rng = new SecureRandom();
+    	byte[] cookieNonce = new byte[16]; // 16 bytes = 128 bits
+    	rng.nextBytes(cookieNonce);
+    	
 		while(true){
-	    	//Wait to receive a message
-    		serverSocket.receive(packet);
-    		
-    		//If user is new, add them, set them up with their name and hash of their PW
-    		
-    		//If user is known, check that they provided the correct PW
-    		//Get the hash matching the provided login name
-    		//If it matches the provided hash, log the user in
-    		
-    		
+	    	Socket clientSocket = serverSocket.accept();
+	    	ClientThread c = new ClientThread(clientSocket, privateKey, cookieNonce);
+	    	c.start();
 		}
 		
 	}
 	
-	private String getList(){
+	// read bytes from a file
+	public static byte[] readByteFromFile(File f) throws IOException{
+		if (f.length() > Integer.MAX_VALUE)
+			System.out.println("File is too large");
+			
+		byte[] buffer = new byte[(int) f.length()];
+		InputStream fis = new FileInputStream(f);;
+		DataInputStream dis = new DataInputStream(fis);
+		dis.readFully(buffer);
+		dis.close();
+		fis.close();
 		
+		return buffer;
+	}
+}
+
+class ClientEntry{
+	
+	public String name;
+	public byte[] pwHash;
+	public InetAddress ip;
+	public boolean available;
+	
+		
+	public ClientEntry(String name, byte[] pwHash, InetAddress ip, boolean available){
+		this.name = name;
+		this.pwHash = pwHash;
+		this.ip = ip;
+		this.available = available;
+	}
+	
+	private void setIP(InetAddress newIP){
+		this.ip = newIP;
+	}
+		
+	private void setAvailable(boolean available){
+		this.available = available;
+	}
+}
+
+
+
+class ClientThread extends Thread {
+	
+	private ObjectInputStream input = null;
+	private ObjectOutputStream output = null;
+	private Socket clientSocket = null;
+	private PrivateKey privateKey;
+	private byte[] cookieNonce;
+	private byte[] nonce1, pwHash, nameBytes;
+	
+	private static final List<ClientEntry> clients = new ArrayList<ClientEntry>();
+
+	public ClientThread(Socket clientSocket, PrivateKey privateKey, byte[] cookieNonce) throws IOException, NoSuchAlgorithmException {
+		this.clientSocket = clientSocket;
+		this.input = new ObjectInputStream(clientSocket.getInputStream());
+		this.output = new ObjectOutputStream(clientSocket.getOutputStream());
+		this.privateKey = privateKey;
+		this.cookieNonce = cookieNonce;
 	}
 
+	public void run() {
+		byte[] initialCreds;
+		try {
+			initialCreds = (byte[])input.readObject();
+			System.out.println("Initial credentials received, sending cookie.");
+			output.writeObject((byte[])makeCookie());
+			System.out.println("Cookie sent, awaiting response.");
+			if(!checkCookie((byte[])input.readObject())){
+				disconnect();
+				return;
+			}
+			
+			System.out.println("Cookie received, IP address verified. Decrypting initial credentials.");
+			Cipher privateCipher = Cipher.getInstance("RSA");
+			privateCipher.init(Cipher.DECRYPT_MODE, privateKey);
+			byte[] decryptedCreds = privateCipher.doFinal(initialCreds);
+			
+			System.out.println("Credentials decrypted. Seperating peices.");
+			nonce1 = Arrays.copyOfRange(decryptedCreds, 0, 16);
+			pwHash = Arrays.copyOfRange(decryptedCreds, 16, 80);
+			nameBytes = Arrays.copyOfRange(decryptedCreds, 80, decryptedCreds.length);
+			String name = new String(nameBytes);
+			
+			System.out.println("Credentials separated for user " + name + ". Checking login history...");
+			
+			
+			
+			
+			
+			
+			while (true) {
+				String line = input.toString();
+				if (line.startsWith("/quit")) {
+					break;
+				}
+			}
+			
+		} 
+		catch (Exception e) {
+		}
+	}
+	
+	private void disconnect() throws IOException{
+		input.close();
+		output.close();
+		clientSocket.close();
+	}
+	
+	private byte[] makeCookie() throws Exception{
+		byte[] ip = this.clientSocket.getInetAddress().getAddress();
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+		outputStream.write(ip);
+		outputStream.write(this.cookieNonce);
+		MessageDigest md = MessageDigest.getInstance("SHA-512");
+		return md.digest(outputStream.toByteArray());
+	}
+	
+	private boolean checkCookie(byte[] providedCookie) throws Exception{
+		byte[] freshCookie = makeCookie();
+		return (providedCookie == freshCookie);
+	}
+	
+	
+	//Returns the index of the clients login/session info if they are known. Returns -1 if they are unknown
+	private int userIsKnown(String name){
+		for(ClientEntry ce:clients){
+			if(ce.name.equals(name)){
+				return clients.indexOf(ce);
+			}
+		}
+		return -1;
+	}
+	
+	
+	
+	
+	
 }
+	
+
+
+
